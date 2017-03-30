2c1226da810d908b0071778f10864cd91b3ed9d5
masbung <masbung@sistrategia.com>
2017-03-30 09:39:35 -0600

    casi final

--- contabilidad/conta_c.c
+++ contabilidad/conta_c.c
@@ -3,51 +3,65 @@
 
 char g_account_name[10][100];
 int g_balance[10];
+int r;
+int accounts_count;
 
 void save(char AccountName[100], int balance) {
     FILE *pFile;
     pFile = fopen("data.txt" , "a"); 
-    fprintf (pFile,"%s",AccountName);
-    fprintf (pFile,"\t %d",balance);
-    fprintf (pFile," \n");
+    fprintf (pFile,"\n%s",AccountName);
+    fprintf (pFile," %d",balance);
     fclose (pFile);                  
 }
 
 void load() {
     char c;
+    r = 0;
+    int i = 0;
+    accounts_count = 0;
     FILE *pFile;
     //char temp_account_name[100];
+    /* pFile = fopen("data.txt" , "r"); */
     pFile = fopen("data.txt" , "r"); 
 
-    if (pFile != NULL) {
-        int r = 0; /* row o line */
+    if(pFile != NULL) {
         do {
+            do  {
+                c = fgetc(pFile);
+                if ( c != EOF && c != ' ' && c != '\n') {
+                    g_account_name[r][i] = c;
+                }
+                i = i + 1;
+            } while (c != EOF && c != ' ' && c != '\n');
+            g_account_name[r][i-1] = '\0';
+            i = 0;
+            /*
             c = fgetc(pFile);
-            int i = 0; /* indice del caracter */
-            while (c != ' ' && c != EOF) {
-                g_account_name[r][i] = c;
-                i = i + 1;
+            while (c >= '0' && c <= '9') {
+                c = fgetc(pFile);
+
             }
-            g_account_name[r][i] = '\0';
-            i = i + 1; /* brinco el espacio */
-            printf("%s\n", g_account_name[r]);
-            /* ahora el numero */
-            while (c >= '0' && c <= '9' && c != EOF) {
-                i = i + 1;
-            }
-            /* ahora el enter */
-            while (c != '\n' && c != EOF) {
-                i = i + 1;
-            } 
+            g_balance[r]=0;
+            */
+            
+            fscanf (pFile,"%d",&g_balance[r]);
+            c = fgetc(pFile);  
+
+            r = r + 1;
         } while (c != EOF);
-        fclose (pFile);
-    }
-    else
-        printf ("No se pudo abrir el archivo. \n");        
+    } 
+    else 
+        printf ("No se pudo abrir el archivo. \n");
+
+    fclose (pFile);
+    accounts_count = r;    
 }
 
 void list_accounts() {
-    load();
+    /* load();  esto NO se debe hacer, porque pierde lo que tenia en memoria */
+    for (int r = 0; r < accounts_count; r++) {
+        printf("%d. %s (%d)\n",r, g_account_name[r], g_balance[r]);
+    }
 }
 
 void add_account() {
@@ -63,7 +77,7 @@ void add_account() {
 
 int main () { 
     int menu_option;     
-    
+    load();
     do {
         printf("Que funcion quieres hacer?\n 1. Listar cuentas \n 2. Agregar cuentas \n 3. Realizar un movimiento \n 4. Salir del programa\n");
         scanf("%d",&menu_option);    
@@ -85,4 +99,3 @@ int main () {
         getchar();
     } while (menu_option != 4);
 } 
-
